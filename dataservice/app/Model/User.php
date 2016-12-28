3439d2dfb4035935f2bd8c197d4cdfeab30d141a
bean <guhaibin1847@gmail.com>
2016-12-29 02:47:56 +0800

    update

--- dataservice/app/Model/User.php
+++ dataservice/app/Model/User.php
@@ -17,12 +17,24 @@ class User extends BaseModel
         return $this->belongsToMany('App\Model\User', 'user_follows', 'user_id', 'follower_id');
     }
 
+    public function _followersCount(){
+        return$this->followers()->count();
+    }
+
     public function follows(){
         return $this->belongsToMany('App\Model\User', 'user_follows', 'follower_id', 'user_id');
     }
 
+    public function _followsCount(){
+        return $this->follows()->count();
+    }
+
     public function articles(){
         return $this->hasMany('App\Model\Article', 'author_id')->where('del_flag', '=', '0');
+    }
+
+    public function _articlesCount(){
+        return $this->articles()->count();
     }
 
     public function checkedArticles(){
@@ -33,12 +45,24 @@ class User extends BaseModel
         return $this->belongsToMany('App\Model\Article', 'user_like_article', 'user_id', 'article_id');
     }
 
+    public function _likeCount(){
+        return $this->likeArticles()->count();
+    }
+
     public function collectArticles(){
         return $this->belongsToMany('App\Model\Article', 'user_collect_article', 'user_id', 'article_id');
     }
 
+    public function _collectCount(){
+        return $this->collectArticles()->count();
+    }
+
     public function subscribeTags(){
         return $this->belongsToMany('App\Model\Tag', 'tag_subscriber', 'subscriber_id', 'tag_id');
+    }
+
+    public function _tagCount(){
+        return $this->subscribeTags()->count();
     }
 
     public function notices(){
