81c5f70ffa3fabab516d08496096022495bdf29f
bean <guhaibin1847@gmail.com>
2016-12-28 23:52:42 +0800

    update

--- web/resources/views/admin/index.blade.php
+++ web/resources/views/admin/index.blade.php
@@ -72,7 +72,7 @@
                         <!-- DOC: Apply "dropdown-dark" class after below "dropdown-extended" to change the dropdown styte -->
                         <li class="dropdown dropdown-user">
                             <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-                                <img alt="" class="img-circle" src="<?php echo env('APP_URL') . session('avatar') ?>" />
+                                <img alt="" class="img-circle" src="<?php echo session('avatar') ?>" />
                                 <span class="username username-hide-on-mobile"><?php echo session('username') ?></span>
                                 <i class="fa fa-angle-down"></i>
                             </a>
